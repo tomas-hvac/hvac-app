2e3b5c9be3bf5e6eb3adf3dd958dd2f7dc05b192
tomas-hvac <thesflooringguy@gmail.com>
2026-06-09 22:24:40 -0700

    Disable hidden orthogonal snap for precision tracing

--- lib/hvac/blueprintRoomTracing.ts
+++ lib/hvac/blueprintRoomTracing.ts
@@ -56,7 +56,7 @@ type BlueprintRoomTraceFinishOptions = {
 };
 
 const TRACE_CLOSE_THRESHOLD_PERCENT = 1;
-const TRACE_CORNER_SNAP_THRESHOLD_PERCENT = 0.2;
+const TRACE_CORNER_SNAP_THRESHOLD_PERCENT = 0;
 
 export function createDefaultBlueprintRoomBoundaryEdges(
   points: BlueprintCalibrationPoint[]
